51d6a1122a7e36c2fa4ac11021083809e4a3ac54
Cedric David <cedric.david@btopenworld.com>
2018-10-04 15:56:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 var sortedRobots = robots.map(function (robot) {
-  if (knownDecepticons.includes(robot.name) {
+  if (knownDecepticons.includes(robot.name)) {
     return Object.assign ( {},
     robot, { alliance: 'decepticon'})
   }else{
